a123c2b3b626c7bb17119342b71b2f10a16e2301
Alastair Lloyd <dex8056@aol.com>
2024-03-06 10:23:23 -0700

    server implemented to display books

--- src/pages/Books/Books.js
+++ src/pages/Books/Books.js
@@ -1,67 +1,25 @@
 import PreviousSearch from "../../components/PreviousSearch"
 import BookCard from "./BookCard"
 import axios from "axios"
-
-
-
+import { useState, useEffect } from 'react'
 
 export default function Books(){
-  // below axios code not implemented as currently not working, site still running of static array
-  axios.get('http://localhost:8000/books')
-  .then(function (response) {
-  // pulls out just the array of books data
-    const sortedBooks = response.data.books
-  // randomizes it to match pages current functionality
-    const randomizedBooks = sortedBooks.sort(() => Math.random() - 0.5)
-    console.log(randomizedBooks)
-  // code below not working as expected WIP
-    const AllBook = () => {
-      return randomizedBooks.map((book, index) => <BookCard key={index} book={book} />)
-  }})
-  
-  const books = [
-    {
-      title: "The Iliad",
-      img: "/img/books/book1.jpg",
-      authorImg: "/img/authors/author1.jpg"
-    },
-    {
-      title: "The Odyssey",
-      img: "/img/books/book2.jpg",
-      authorImg: "/img/authors/author1.jpg"
-    },
-    {
-      title: "Gates Of Fire",
-      img: "/img/books/book3.jpg",
-      authorImg: "/img/authors/author2.jpg"
-    },
-    {
-      title: "Meditations",
-      img: "/img/books/book4.jpg",
-      authorImg: "/img/authors/author3.jpg"
-    },
-    {
-      title: "The Daily Stoic",
-      img: "/img/books/book5.jpg",
-      authorImg: "/img/authors/author3.jpg"
-    },
-    {
-      title: "The Fellowship Of The Ring",
-      img: "/img/books/book6.jpg",
-      authorImg: "/img/authors/author6.jpg"
-    },
-    {
-      title: "Norse Mythology",
-      img: "/img/books/book7.jpg",
-      authorImg: "/img/authors/author4.jpg"
-    },
-    {
-      title: "Pet Sematary",
-      img: "/img/books/book8.jpg",
-      authorImg: "/img/authors/author5.jpg"
-    },
-  ].sort(() => Math.random() - 0.5)
+  // sets `book` to an empty array and it returns a function called `setBooks` to set the value of `books`
+  const [books, setBooks] = useState([])
+  const getBooks = () => {
+    axios.get('http://localhost:8000/books')
+      .then((response) => {
+        const sortedBooks = response.data.books
+        // randomizes it to match pages current functionality
+        const randomizedBooks = sortedBooks.sort(() => Math.random() - 0.5)
+        setBooks(randomizedBooks)
+      })
+  }
 
+  useEffect(() => {
+    getBooks()
+  }, [])
+  
   const AllBooks = () => {
     return books.map((book, index) => <BookCard key={index} book={book} />)
   }
